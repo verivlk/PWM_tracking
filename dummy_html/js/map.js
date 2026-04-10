c713e53572289f3eaadde476ea07a3145a2661d0
francescopicotto <francesco.picotto101@alu.ulpgc.es>
2026-04-10 17:23:55 +0100

    cleared repo before migrating to Angular

--- dummy_html/js/map.js
+++
@@ -1,9 +0,0 @@
-// js/map.js
-
-document.addEventListener('DOMContentLoaded', async function () {
-    await renderMap();
-});
-
-async function renderMap() {
-    const map = await initMap('#map-panel');
-}
