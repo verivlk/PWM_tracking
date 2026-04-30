2c67d08047f406c9025a2e8b047db5827fa13d33
francescopicotto <francesco.picotto101@alu.ulpgc.es>
2026-04-30 18:49:26 +0100

    made dark mode work and uploading dark mode setting to db

--- my-first-app/src/app/components/ui/toggle-switch/toggle-switch.ts
+++ my-first-app/src/app/components/ui/toggle-switch/toggle-switch.ts
@@ -1,17 +1,22 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-toggle-switch',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './toggle-switch.html',
   styleUrls: ['./toggle-switch.css']
 })
 export class ToggleSwitchComponent {
+  // Riceve lo stato (acceso/spento) dal padre
   @Input() checked: boolean = false;
+
+  // Emette il nuovo stato quando l'utente clicca
   @Output() changed = new EventEmitter<boolean>();
 
   onToggle(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.changed.emit(input.checked);
+    const inputElement = event.target as HTMLInputElement;
+    this.changed.emit(inputElement.checked);
   }
 }
